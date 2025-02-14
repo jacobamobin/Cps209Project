37b1f72fee9388311d65ddbf72838d4ae603dab7
Jacob Mobin <jacobamobin@gmail.com>
2025-02-13 21:05:06 -0500

    Update Main.java

---
+++ Main.java
@@ -0,0 +1,30 @@
+// NAME: Jacob Mobin
+// STUDENTNUM: 501330782
+// TMUEMAIL: jacob.mobin@torontomu.ca
+// Class: CPS209 SECTION: 4
+// DATE: Feb 13, 2025
+
+/* PROJECT DESCRIPTION:
+ * 
+ * PROBLEM: You are running a massive company but your internal emails and memos keep getting leaked.
+ * How do we find out whos leaking these memos, while the memos look (nearly) identical for every recipient.
+ * Well, in 2008, tesla had the same problem, and they used a form of Steganography (Hiding secret messages in a message) to solve it
+ * They sent out different emails to everyone in the company which has a unique combo of single and double spaces so that it can be traced back to a single recipient.
+ * So my project will recreate this, and allow you to trace individual unique doccuments back to the individual it was sent to
+ */
+
+ /* HOW TO USE:
+  * 
+  */
+
+/* HOW IT WORKS
+ * 
+ * 
+ * 
+ * 
+ */
+class Main {
+    public static void main(String[] args) {
+        
+    }
+}
